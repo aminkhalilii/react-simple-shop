ea1c44f6eb1d8d77dd7183c61a0555349a7bee19
aminkhalilii <aminkhalili430@yahoo.com>
2024-08-14 12:28:17 +0330

    add cart

--- src/contexts/ShopContext.js
+++ src/contexts/ShopContext.js
@@ -1,40 +1,38 @@
 import { createContext, useState } from "react";
-
 export const ShopContext = createContext(null);
 export const ShopContextProvider = (props) => {
-	const [cart, setCart] = useState([]);
-	const addToCart = (id) => {
-		if (cart.find((item) => item.id === id)) {
-			setCart(
-				cart.map((item) => {
-					if (item.id === id) {
-						return { ...item, count: item.count + 1 };
-					}
-					return item;
-				})
-			);
-		} else {
-			setCart([...cart, { id: id, count: 1 }]);
-		}
-	};
-	const removeFromCart = (id) => {
-		setCart(
-			cart.map((item) => {
-				if (item.id === id)
-					return { ...item, count: item.count > 0 ? item.count - 1 : 0 };
-				return item;
-			})
-		);
-	};
-	const contextValues = {
-		cart,
-		addToCart,
-		removeFromCart,
-	};
-	return (
-		<ShopContext.Provider value={contextValues}>
-			{" "}
-			{props.children}
-		</ShopContext.Provider>
-	);
+  const [cart, setCart] = useState([]);
+  const addToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) => {
+          if (item.id === id) {
+            return { ...item, count: item.count + 1 };
+          }
+          return item;
+        })
+      );
+    } else {
+      setCart([...cart, { id: id, count: 1 }]);
+    }
+  };
+  const removeFromCart = (id) => {
+    setCart(
+      cart.map((item) => {
+        if (item.id === id)
+          return { ...item, count: item.count > 0 ? item.count - 1 : 0 };
+        return item;
+      })
+    );
+  };
+  const contextValues = {
+    cart,
+    addToCart,
+    removeFromCart,
+  };
+  return (
+    <ShopContext.Provider value={contextValues}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import { ShopContext } from "../contexts/ShopContext";
+import { PRODUCTS } from "../data/products";
+import Product from "../components/Product" 
+
 const Cart = () => {
-	return <div>Cart</div>;
+	const {cart} = useContext(ShopContext);
+	console.log(cart);
+	
+	return <div>
+		<h1 className="text-center text-3xl my-5">Your Cart Items</h1>
+			<div className="flex flex-wrap justify-center gap-6 px-6">
+				{PRODUCTS.map((product) => {
+					if(cart.some((cartItem)=>cartItem.id === product.id && cartItem.count>0)){
+						return	<Product key={product.id} product={product} />
+					}
+				})}
+			</div>
+
+	</div>;
 };
 
 export default Cart;
